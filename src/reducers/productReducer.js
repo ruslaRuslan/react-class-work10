19ea21f92df034deed6835ca27d3433210c74e7d
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-24 15:22:09 +0400

    productReducer.js hazir

--- src/reducers/productReducer.js
+++ src/reducers/productReducer.js
@@ -1,19 +1,21 @@
-const productReducer = (products, action) =>{
-    switch(action.type){
-        case 'getproducts':
-            return action.payload
-            case'deleteproduct':
-            return products.filter((product)=> product.id !== action.payload)
-            case'editproduct':
-            return products.map((product)=>{
-                if(product.id=== action.payload){
-                    return{...product, title: prompt('enter new name')}
-                }
-                return product
-            })
-            case'deletefirst':
-            return products.slice(1)
-            case'deleteiphones':
-            return products.filter((product)=> !product.toLowerCase().startWith('iphone'))
-    }
-}
+const productReducer = (products, action) => {
+  switch (action.type) {
+    case "getproducts":
+      return action.payload;
+    case "deleteproduct":
+      return products.filter((product) => product.id !== action.payload);
+    case "editproduct":
+      return products.map((product) => {
+        if (product.id === action.payload) {
+          return { ...product, title: prompt("enter new name") };
+        }
+        return product;
+      });
+    case "deletefirst":
+      return products.slice(1);
+    case "deleteiphones":
+      return products.filter(
+        (product) => !product.toLowerCase().startWith("iphone")
+      );
+  }
+};
