639f5e8bedb5ba242b39d2f7ca18b47f5f023621
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-24 15:14:40 +0400

    editproduct

--- src/reducers/productReducer.js
+++ src/reducers/productReducer.js
@@ -2,5 +2,14 @@ const productReducer = (products, action) =>{
     switch(action.type){
         case 'getproducts':
             return action.payload
+            case'deleteproduct':
+            return products.filter((product)=> product.id !== action.payload)
+            case'editproduct':
+            return products.map((product)=>{
+                if(product.id=== action.payload){
+                    return{...product, title: prompt('enter new name')}
+                }
+                return product
+            })
     }
 }
