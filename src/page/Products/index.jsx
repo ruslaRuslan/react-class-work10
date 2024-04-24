229a4768ac76538c4dfe7b596e6996f9986642c7
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-24 14:46:19 +0400

    useEffect

--- src/page/Products/index.jsx
+++ src/page/Products/index.jsx
@@ -1,4 +1,23 @@
+import axios from "axios";
+import { useEffect, useReducer } from "react";
+import productReducer from "../../reducers/productReducer";
+
+const url = "https://dummyjson.com/products";
+
+const ProductsPage = () => {
+  const [products, dispatch] = useReducer(productReducer, []);
+  useEffect(() => {
+    axios.get(url).then(({ data }) => {
+      dispatch({ type: "getproducts", payload: data.products });
+    });
+  },[]);
 
 
 
-const url = 'https://dummyjson.com/products'
+
+
+
+
+
+
+};
